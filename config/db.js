f56bc2de976376bd0e68dfda9469809b60b397ec
Pratham Singh <107633022+prathamsingh0116@users.noreply.github.com>
2026-07-19 19:37:43 +0530

    Refactor MongoDB connection function
    Updated MongoDB connection logging for clarity.

--- config/db.js
+++ config/db.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
-// mongoose.set("strictQuery", true);
+
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-
-        console.log("✅ MongoDB Connected Successfully");
-    } catch (err) {
-        console.error("❌ MongoDB Connection Failed");
-        console.error(err.message);
-
-        process.exit(1);
-    }
+  try {
+    console.log("Connecting to MongoDB...");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ MongoDB Error:", err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
